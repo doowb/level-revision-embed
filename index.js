cf78b19eaeb0d97dfbeb5f31ad44d672759c8938
Brian Woodward <brian.woodward@gmail.com>
2015-03-30 12:53:36 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * level-revision-embed <https://github.com/doowb/level-revision-embed>
+ *
+ * Copyright (c) 2015 Brian Woodward.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
